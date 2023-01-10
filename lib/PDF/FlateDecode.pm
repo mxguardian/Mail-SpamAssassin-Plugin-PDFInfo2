068e61b91e1f3501674489c122cb76310f9bc95a
Kent Oyer <kent.oyer@gmail.com>
2023-01-10 10:58:35 -0600

    Improve FlateDecode

--- lib/PDF/FlateDecode.pm
+++ lib/PDF/FlateDecode.pm
@@ -9,41 +9,40 @@ sub decode {
     $data = uncompress($data);
     return $data unless defined($params) && defined($params->{'/Predictor'});
 
+    my $out;
     my $predictor = $params->{'/Predictor'};
-
     if ( $predictor == 2 ) {
         die "TIFF Predictor not implemented";
     } elsif ( $predictor >= 10 ) {
 
-        my $columns = $params->{'/Columns'};
+        # PNG Predictor https://www.rfc-editor.org/rfc/rfc2083#section-6
+        my $columns = $params->{'/Columns'} + 1;
         my $length = length($data);
-        my $out;
 
         my @prior = (0) x $columns;
-        for( my $i=0; $i<$length; $i+=($columns+1) ) {
-            my $template = 'x'.$i.'C'.($columns+1);
-            my @row = unpack($template,$data);
-            my $alg = shift(@row);
+        for( my $i=0; $i<$length; $i+=$columns ) {
             my @out;
+            my @row = unpack("x$i C$columns",$data);
+            my $alg = shift(@row);
 
-            for( my $x=0; $x<scalar(@row);$x++) {
-                if ( $alg == 2 ) {
-                    push(@out,($row[$x]+$prior[$x])%256);
-                } else {
-                    die "Unknown algorithm: $alg";
-                }
+            if ( $alg == 2 ) {
+                # PNG "Up" Predictor
+                push(@out,($row[$_]+$prior[$_])%256) for (0..$#row)
+            } else {
+                die "PNG algorithm $alg not implemented";
             }
 
             $out .= pack('C*',@out);
             # printf "i=$i prior=%s row=%s out=%s\n",join(',',@prior),join(',',@row),join(',',@out);
 
-            @prior = ( @out );
+            @prior = @out;
         }
-        return $out;
 
     } else {
         die "Unknown predictor $predictor";
     }
+
+    return $out;
 
 }
 
